75c6c413d7344faa5e2a69c8bfd35332ff77ad66
Josiah Berkebile <praenato14@gmail.com>
2019-10-26 20:14:59 -0500

    create mercury coding environment and implement entropy predicate

--- ID3/mercury/sequential/id3-seq.m/id3_seq.m
+++ ID3/mercury/sequential/id3-seq.m/id3_seq.m
@@ -3,8 +3,22 @@
 :- import_module io.
 :- pred main(io :: di, io :: uo) is det.
 :- implementation.
-:- import_module string, list.
+:- import_module string, list, integer, float, math.
+
+:- pred negate_f(float :: in, float :: out) is det.
+negate_f(Num, NegatedNum) :-
+  NegatedNum = -Num.
+
+:- pred entropy(int :: in, list(int) :: in, float :: in, float :: out) is det.
+entropy(NumRecords, ValueFreqs, LogBase, Entropy) :-
+  ItemProbs = map(func(Freq) = float(Freq) / float(NumRecords), ValueFreqs)
+  , negate_f(
+      foldl(func(P, A) = (P * log(LogBase, P)) + A, ItemProbs, 0.0), Entropy
+    ).
 
 main(!IO) :-
   io.command_line_arguments(Args, !IO),
-  io.print(Args, !IO).
+  io.print(Args, !IO),
+  io.nl(!IO),
+  entropy(7, [3, 2, 2], 2.0, Entropy),
+  io.print(Entropy, !IO).
